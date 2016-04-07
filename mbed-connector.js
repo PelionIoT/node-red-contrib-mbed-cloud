2e9654b56607e1fd6dba6f58d796d9e9a4c770d9
Austin <austin.blackstone@arm.com>
2016-04-07 02:42:54 +0000

    safety commit

--- mbed-connector.js
+++ mbed-connector.js
@@ -1,5 +1,5 @@
 module.exports = function(RED) {
-    function LowerCaseNode(config) {
+    function Connector(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
@@ -7,5 +7,5 @@ module.exports = function(RED) {
             node.send(msg);
         });
     }
-    RED.nodes.registerType("mbed-connector",LowerCaseNode);
+    RED.nodes.registerType("mbed-connector",Connector);
 }
