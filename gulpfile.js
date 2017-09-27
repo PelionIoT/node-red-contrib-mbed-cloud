c3f768bbdd61c2d44383151bd75d9a97844ac2b8
Rob Moran <rob.moran@arm.com>
2017-09-27 10:50:04 +0100

    Typescript PoC

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var path        = require("path");
+var del         = require("del");
+var merge       = require('merge2');
+var tslint      = require("tslint");
+var gulp        = require("gulp");
+var gulpTs      = require("gulp-typescript");
+var gulpTslint  = require("gulp-tslint");
+
+var srcDir = "src";
+var srcFiles = srcDir + "/**/*.ts";
+var copyFiles = srcDir + "/**/*.html";
+var distDir = "dist";
+var typesDir = "types";
+var watching = false;
+
+function handleError() {
+    if (watching) this.emit("end");
+    else process.exit(1);
+}
+
+// Set watching
+gulp.task("setWatch", function() {
+    watching = true;
+});
+
+// Clear built directories
+gulp.task("clean", function() {
+    return del([distDir, typesDir]);
+});
+
+// Lint the source
+gulp.task("lint", function() {
+    var program = tslint.Linter.createProgram("./");
+
+    gulp.src(srcFiles)
+    .pipe(gulpTslint({
+        program: program,
+        formatter: "stylish"
+    }))
+    .pipe(gulpTslint.report({
+        emitError: !watching
+    }))
+});
+
+// Copy HTML
+gulp.task("copy", ["clean"], function() {
+    return gulp.src(copyFiles)
+    .pipe(gulp.dest(distDir))
+    .on("error", handleError);
+});
+
+// Build TypeScript source into CommonJS Node modules
+gulp.task("compile", ["copy"], function() {
+    var tsResult = gulp.src(srcFiles)
+    .pipe(gulpTs({
+        target: "es5",
+        module: "commonjs",        
+        alwaysStrict: true,
+        noEmitOnError: true,
+        noUnusedLocals: true,
+        declaration: true,
+        noUnusedParameters: true
+    }))
+    .on("error", handleError);
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest(typesDir)),
+        tsResult.js.pipe(gulp.dest(distDir))
+    ]);
+});
+
+gulp.task("watch", ["setWatch", "default"], function() {
+    gulp.watch([srcFiles, copyFiles], ["default"]);
+});
+
+gulp.task("default", ["lint", "compile"]);
